0111a10cc7e75317953fc999e61e39fb31209a87
julianknodt <julianknodt@gmail.com>
2022-07-02 00:43:12 -0700

    Switch to clap, add more functionality

--- readme.md
+++ readme.md
@@ -1,7 +1,23 @@
 # Normalize OBJ
 
-It's just a small script so clone it manually and run:
+It's often the case that you want to normalize an OBJ file to the unit box `[-1, -1, -1],
+[1,1,1]`, or move the average of its vertices to 0. This is a small tool for doing so.
+
+It also comes with a utility to normalize one mesh to another's bounding box or center.
+
+## Installation and Usage
 
 ```
-cargo run <paths to OBJ files>
+git clone git@github.com:JulianKnodt/normalize_obj.git
+cd normalize_obj
+cargo install .
+...
+normalize-obj --src <SRC_OBJ_FILE> --output <OUTPUT_OBJ>.obj --method [centroid | aabb] \
+  --target <REFERENCE_OBJ_FILE?>
 ```
+
+## Contributing
+
+If you have any other additional functionality you want added, please feel free to submit a pull
+request!
+
--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,214 @@
+use clap::Parser;
+
 use std::{
-  fs::File,
-  io::{BufRead, BufReader, BufWriter, Write},
+    fmt,
+    fs::File,
+    io::{BufRead, BufReader, BufWriter, Write},
 };
 
+type Vec3 = [f64; 3];
+
+/// Utility to normalize an OBJ mesh to the center, and fit it within to the unit box, or within
+/// another mesh's bounding box.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Meshes to normalize.
+    #[clap(short, long, value_parser)]
+    src: String,
+    /// How should the mesh be normalized? Either fixing the AABB, or the average of all the
+    /// vertices.
+    #[clap(short, long, value_parser, default_value_t = NormalizeKind::AABB)]
+    method: NormalizeKind,
+    /// Which mesh to normalize to, if any.
+    #[clap(short, long, value_parser)]
+    target: Option<String>,
+
+    /// Output file name
+    #[clap(short, long, value_parser)]
+    output: Option<String>,
+
+    /// Perform operation in place (can be specified in addition to outputting a file).
+    #[clap(long)]
+    in_place: bool,
+}
+
 fn main() {
-  let iter = std::env::args()
-    .skip(1)
-    .filter(|it| it.ends_with(".obj"))
-    .filter(|it| !it.starts_with("normed_"));
-  for obj_file in iter {
-    let out = normalize(&obj_file).expect("Failed to normalize OBJ file");
-    let out_file = File::create(&obj_file).expect("Failed to create file");
-    let mut dst = BufWriter::new(out_file);
+    let args = Args::parse();
+
+    let out = normalize(&args.src, args.target.as_ref(), args.method);
+    let mut out_files = vec![];
+    if args.in_place {
+        let out_file = File::create(&args.src).expect("Failed to replace original file");
+        out_files.push(BufWriter::new(out_file));
+    }
+    if let Some(output) = args.output {
+        let out_file = File::create(&output).expect("Failed to create output file");
+        out_files.push(BufWriter::new(out_file));
+    }
     for l in out {
-      write!(dst, "{}\n", l).expect("Write failed");
+        for f in out_files.iter_mut() {
+            write!(f, "{}\n", l).expect("Write failed");
+        }
     }
-  }
 }
 
-fn normalize(file_name: &str) -> Result<impl Iterator<Item = String>, &'static str> {
-  let file = File::open(file_name).map_err(|_err| "Failed to open file")?;
-  let reader = BufReader::new(file);
-  let mut read_lines: Vec<Option<String>> = vec![];
-  let mut points: Vec<(f64, f64, f64)> = vec![];
-
-  for l in reader.lines() {
-    let l = l.map_err(|_err| "Failed to read line")?;
-    if Some("v") != l.trim_start().split_whitespace().next() {
-      read_lines.push(Some(l));
-      continue;
+fn read_vertices(file_name: &String) -> Vec<Vec3> {
+    let file = File::open(file_name).expect("Failed to open file");
+    let reader = BufReader::new(file);
+    let mut points: Vec<Vec3> = vec![];
+    for l in reader.lines() {
+        let l = l.expect("Failed to read line");
+        match l.trim().split_whitespace().collect::<Vec<_>>().as_slice() {
+            ["v", x, y, z] => {
+                points.push([x.parse().unwrap(), y.parse().unwrap(), z.parse().unwrap()]);
+            }
+            _ => continue,
+        }
     }
-    read_lines.push(None);
-    match l.trim().split_whitespace().collect::<Vec<_>>().as_slice() {
-      ["v", x, y, z] => {
-        points.push((x.parse().unwrap(), y.parse().unwrap(), z.parse().unwrap()));
-      },
-      v => panic!("Unexpected matched sequence {:?}", v),
+    points
+}
+
+/// Normalize the given file to unit box or another file.
+fn normalize(
+    file_name: &str,
+    to_match: Option<&String>,
+    kind: NormalizeKind,
+) -> impl Iterator<Item = String> {
+    let file = File::open(file_name).expect("Failed to open file");
+    let to_match = to_match.map(|tm| read_vertices(tm));
+    let reader = BufReader::new(file);
+    // keep track all lines read, so that can write out the file later
+    let mut read_lines: Vec<Option<String>> = vec![];
+    let mut points: Vec<Vec3> = vec![];
+
+    for l in reader.lines() {
+        let l = l.expect("Failed to read line");
+        match l.trim().split_whitespace().collect::<Vec<_>>().as_slice() {
+            ["v", x, y, z] => {
+                points.push([x.parse().unwrap(), y.parse().unwrap(), z.parse().unwrap()]);
+                read_lines.push(None);
+            }
+            ["v", ..] => panic!("Unhandled syntax: {:?}", l),
+            _unknown => read_lines.push(Some(l)),
+        }
     }
-  }
-  let average = avg(&points);
-  for p in points.iter_mut() {
-    *p = sub(*p, average);
-  }
-  let max_norm = points
-    .iter()
-    .map(|p| norm(p))
-    .max_by(|a, b| a.partial_cmp(b).unwrap())
-    .unwrap();
-  println!("Shifted by {:?}, scaled by {:?}", average, max_norm);
-  assert!(max_norm.is_finite());
-  for p in points.iter_mut() {
-    *p = kdiv(*p, max_norm);
-  }
-  let iter = read_lines
-    .iter_mut()
-    .filter(|l| l.is_none())
-    .zip(points.drain(..));
-  for (l, (x, y, z)) in iter {
-    *l = Some(String::from(format!("v {:?} {:?} {:?}", x, y, z)));
-  }
-  Ok(read_lines.into_iter().map(Option::unwrap))
+    let (center, scale) = kind.center_scale(&points);
+    let map_to = to_match.map(|tm| kind.center_scale(&tm));
+    for p in points.iter_mut() {
+        *p = sub(*p, center);
+        *p = div(*p, scale);
+        if let Some((new_center, new_scale)) = map_to {
+            *p = mul(*p, new_scale);
+            *p = add(*p, new_center);
+        }
+    }
+    let iter = read_lines
+        .iter_mut()
+        .filter(|l| l.is_none())
+        .zip(points.into_iter());
+    for (l, [x, y, z]) in iter {
+        *l = Some(String::from(format!("v {:?} {:?} {:?}", x, y, z)));
+    }
+    read_lines.into_iter().map(Option::unwrap)
 }
 
-fn avg(vec: &[(f64, f64, f64)]) -> (f64, f64, f64) {
-  let mut x_acc = 0.;
-  let mut y_acc = 0.;
-  let mut z_acc = 0.;
-  let n = vec.len() as f64;
-  for (x, y, z) in vec {
-    x_acc += x / n;
-    y_acc += y / n;
-    z_acc += z / n;
-  }
-  (x_acc, y_acc, z_acc)
+fn avg(vec: &[Vec3]) -> Vec3 {
+    let mut x_acc = 0.;
+    let mut y_acc = 0.;
+    let mut z_acc = 0.;
+    let n = vec.len() as f64;
+    for [x, y, z] in vec {
+        x_acc += x / n;
+        y_acc += y / n;
+        z_acc += z / n;
+    }
+    [x_acc, y_acc, z_acc]
 }
 
-fn sub((a, b, c): (f64, f64, f64), (x, y, z): (f64, f64, f64)) -> (f64, f64, f64) {
-  (a - x, b - y, c - z)
+fn bounding_box(vs: &[Vec3]) -> (Vec3, Vec3) {
+    let mut lx = f64::INFINITY;
+    let mut ly = f64::INFINITY;
+    let mut lz = f64::INFINITY;
+    let mut hx = f64::NEG_INFINITY;
+    let mut hy = f64::NEG_INFINITY;
+    let mut hz = f64::NEG_INFINITY;
+    for &[x, y, z] in vs {
+        lx = lx.min(x);
+        ly = ly.min(y);
+        lz = lz.min(z);
+        hx = hx.max(x);
+        hy = hy.max(y);
+        hz = hz.max(z);
+    }
+    ([lx, ly, lz], [hx, hy, hz])
 }
 
-fn norm((a, b, c): &(f64, f64, f64)) -> f64 { (a * a + b * b + c * c).sqrt() }
+fn add([a, b, c]: Vec3, [x, y, z]: Vec3) -> Vec3 {
+    [a + x, b + y, c + z]
+}
 
-fn kdiv((a, b, c): (f64, f64, f64), k: f64) -> (f64, f64, f64) { (a / k, b / k, c / k) }
+fn sub([a, b, c]: Vec3, [x, y, z]: Vec3) -> Vec3 {
+    [a - x, b - y, c - z]
+}
+
+fn div([a, b, c]: Vec3, [x, y, z]: Vec3) -> Vec3 {
+    [a / x, b / y, c / z]
+}
+
+fn mul([a, b, c]: Vec3, [x, y, z]: Vec3) -> Vec3 {
+    [a * x, b * y, c * z]
+}
+
+fn norm([a, b, c]: &Vec3) -> f64 {
+    (a * a + b * b + c * c).sqrt()
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum NormalizeKind {
+    AABB,
+    Centroid,
+}
+
+impl NormalizeKind {
+    fn center_scale<'a>(self, vs: &[Vec3]) -> (Vec3, Vec3) {
+        match self {
+            NormalizeKind::AABB => {
+                let (ll, ur) = bounding_box(vs);
+                let extent = sub(ur, ll);
+                let half_extent = div(extent, [2., 2., 2.]);
+                let center = add(ll, half_extent);
+                (center, half_extent)
+            }
+            NormalizeKind::Centroid => {
+                let center = avg(vs);
+                let max_k = vs
+                    .iter()
+                    .map(|v| norm(&sub(*v, center)))
+                    .max_by(|a, b| a.partial_cmp(b).unwrap())
+                    .unwrap_or(1.);
+                (center, [max_k, max_k, max_k])
+            }
+        }
+    }
+}
+
+impl fmt::Display for NormalizeKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            NormalizeKind::AABB => write!(f, "AABB"),
+            NormalizeKind::Centroid => write!(f, "Centroid"),
+        }
+    }
+}
+
+impl std::str::FromStr for NormalizeKind {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, String> {
+        match s.to_lowercase().as_str() {
+            "aabb" => Ok(NormalizeKind::AABB),
+            "centroid" => Ok(NormalizeKind::Centroid),
+            x => Err(format!("Unknown normalization: {:?}", x)),
+        }
+    }
+}
